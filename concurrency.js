4d785aa39c4b8c63d600cec06f17074d7c508a44
AlpsMonaco <55621471+AlpsMonaco@users.noreply.github.com>
2021-07-09 16:23:28 +0800

    ver 1

---
+++ concurrency.js
@@ -0,0 +1,116 @@
+// usage :
+// C.add(asyncFuncProtype,...args)
+// C.wait().then(()=>{console.log("all done")})
+// asyncFuncProtype must be a async func,otherwise there might be uncentain error.
+// look demo  for more detail.
+
+const C = (function () {
+    let maxRunning = 5
+    let currentRunning = 0
+    let iterator = {
+        queue: [], cursor: -1,
+
+        add: function (asyncFunc, ...args) {
+            this.queue.push({ f: asyncFunc, args: args })
+        },
+
+        reset: function () {
+            this.cursor = -1; this.queue = []
+        },
+
+        next: function () {
+            return ++this.cursor > iterator.queue.length - 1 ? null : iterator.queue[this.cursor]
+        },
+    }
+
+    function worker() {
+        setTimeout(() => {
+            let o = iterator.next()
+            if (!o) {
+                currentRunning--
+                console.log("die")
+                return
+            }
+
+            o.f(...o.args).then((...args) => {
+                worker()
+            }).catch((e) => { console.error(e) })
+        }, 100)
+    }
+
+    function newJob() {
+        if (currentRunning >= maxRunning) return
+        console.log("new Worker")
+        currentRunning++
+        worker()
+    }
+
+    return {
+        // add async function and args.
+        // 
+        add: function (asyncFunc, ...args) {
+            iterator.add(asyncFunc, ...args)
+            newJob()
+        },
+        wait: async function () {
+            for (; ;) {
+                await new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve()
+                    }, 1000)
+                })
+
+                if (currentRunning == 0) {
+                    break
+                }
+            }
+        },
+
+        // set max concurrency
+        setMaxRunning: function (num) {
+            maxRunning = num
+        },
+
+        isDone: function () {
+            return currentRunning == 0
+        }
+    }
+})()
+
+
+function demo() {
+    function timeToPrint(t) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log(t)
+                resolve()
+            })
+        })
+    }
+
+    setTimeout(() => {
+        for (let i = 1; i < 100; i++) {
+            let t = i
+            C.add(timeToPrint, t)
+        }
+    }, 100)
+
+    setTimeout(() => {
+        for (let i = 1; i < 100; i = i + 5) {
+            let t = i
+            C.add(timeToPrint, t)
+        }
+    }, 2000)
+
+    setTimeout(() => {
+        for (let i = 1; i < 100; i = i + 3) {
+            let t = i
+            C.add(timeToPrint, t)
+        }
+    }, 5000)
+
+
+    C.wait().then(() => {
+        console.log("all done")
+    })
+}
